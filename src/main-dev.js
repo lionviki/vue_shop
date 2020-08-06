a61c7c8c93fe4294a85d1fb460fb5dbd573431de
viking <1207690847@qq.com>
2020-08-06 22:41:52 +0800

    项目最终优化

---
+++ src/main-dev.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+// 导入字体图标库
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入axios
+import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+// 导入nprogress包 对应的js和css
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+// 注册富文本编辑器 全局可用
+Vue.use(VueQuillEditor)
+
+Vue.prototype.$http = axios
+// 设置基准路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request拦截器中展示进度条 NProgress.start()
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 在response拦截器中隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+
+// 注册全局的日期过滤器
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- src/main-prod.js
+++ src/main-prod.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+// import './plugins/element.js'
 // 导入字体图标库
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
@@ -11,21 +11,25 @@ import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
-// 导入富文本编辑器样式
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
+// 导入nprogress包 对应的js和css
+import NProgress from 'nprogress'
 // 注册富文本编辑器 全局可用
 Vue.use(VueQuillEditor)
 
 Vue.prototype.$http = axios
 // 设置基准路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request拦截器中展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
-
+// 在response拦截器中隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
